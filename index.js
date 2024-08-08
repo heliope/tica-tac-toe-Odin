214d9775bd670abbb2493b2fcf24d0ecfae27377
heliope <heliopereira-7@hotmail.com>
2024-08-08 17:06:21 +0100

    começo da lógica de jogo

--- index.js
+++ index.js
@@ -1,165 +1,341 @@
 // JOGO - CRIA JOGADORES E MARCAS
 
 // FACTORY FUNCTION
-function createGameController() {
+function Gameboard() {
+
+    // DIMENSÕES DO TABULEIRO
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    // CONSTRUÇÃO DO TABULEIRO
+    for (let i = 0; i < rows; i++) {
+
+        board[i] = [];
+
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+    
+    // CLOSURE -RETORNA VARIAVEL BOARD
+    const getGameBoard = () => board
+
+    // ADICIONA MARK DE UM JOGADOR NO TABULEIRO
+    const placeMark = ( column, row, mark) => {
+
+        // VERIFICA SE ESTÁ DISPONIVEL
+        if ( board[column][row].getMark() === "") {
+
+            if ( mark === "circle") {
+
+                board[column][row].addMark("O");
+            }
+
+            else {
+
+                board[column][row].addMark("X");
+            }
+
+        }
+    }
+
+    // RETORNA UM OBJETO
+    return {getGameBoard, placeMark};
+}
+
+// CÉLULA QUE CONTROLA AS CÉLULAS DO TABULEIRO E VERIFICA O SEU PREENCHIMENTO
+function Cell() {
+
+    // INICIALMENTE O TABULEIRO NAO ESTÁ PREENCHIDO
+    let mark = '';
+
+    const addMark = (playerMark) => {
+
+        mark = playerMark 
+    };
+
+    // DEVOLVE O VALOR DA CÉLULA - VARIAVEL PRIVADA
+    const getMark = () => mark;
+
+    return { addMark, getMark};
+}
+
+// CRIAÇÃO DOS JOGADORES
+function checkPlayerName ( inputId, errorMessageId, buttonEnterId) {
+        
+    const input = document.getElementById(inputId);
+    const errorMessage = document.getElementById(errorMessageId);
+    
+    // O NOME DO JOGADOR DEVE TER ENTRE 3 A 15 LETRAS
+    const regexInput = /^[\p{L}\p{N}]{3,15}$/u;
+
+    // CLOSURE - LÓGICA DE VALIDAÇÃO DO NOME DO JOGADOR
+    const checkName = () => {
+        
+        const inputUserRegex = regexInput.test(input.value);
+
+        if( input.value.length < 3 || input.value.length > 15 ) {
+
+            errorMessage.textContent = "Your name should be between 3 and 15 characters";
+            errorMessage.className = "errorMessageIncorrect";
+        }
+
+        else if( !inputUserRegex ) {
+
+            errorMessage.textContent = "Incorrect characters! Please try again";
+            errorMessage.className = "errorMessageIncorrect";
+        }
+
+        else {
+
+            errorMessage.textContent= "Correct Names! Good Luck!";
+            errorMessage.className = "errorMessageCorrect";
+        }
+
+        // INSTANCIA A FUNÇÃO DE DESBLOQUEAR O BOTÃO DE ENTRADA
+        const validatorButton = checkButton(buttonEnterId);
+        
+        validatorButton.validateButton();
+    }
+
+    // EXECUTA A FUNÇÃO CHECKNAME
+    function attachEventListener() {
+
+        input.addEventListener("input",checkName);
+    }
+
+    function deleteEventListener() {
+
+        input.removeEventListener("input",checkName);
+    }
+
+    // RETORNA O VALOR DO JOGADOR
+    function getName() {
+
+        return input.value;
+    }
+
+    return  {attachEventListener, deleteEventListener, getName};
+}
+
+// DESBLOQUEIA O BOTÃO DE ENTRADA
+function checkButton( buttonEnterId) {
+
+    const regexMessagePlayer1 = document.getElementById("regexMessagePlayer1");
+    const regexMessagePlayer2 = document.getElementById("regexMessagePlayer2");
+    const buttonEnter = document.getElementById(buttonEnterId);
+      
+    //  CLOSURE - LÓGICA DE VALIDAÇÃO
+    const validateButton = () => {
+
+        if( regexMessagePlayer1.classList.contains("errorMessageCorrect") && regexMessagePlayer2.classList.contains("errorMessageCorrect")) {
+
+            buttonEnter.removeAttribute("disabled");
+            buttonEnter.classList.remove("none");
+            buttonEnter.classList.add("enter");
+        }
+
+        else {
+
+            buttonEnter.setAttribute("disabled", "true");
+            buttonEnter.classList.remove("enter");
+            buttonEnter.classList.add("none");
+        }
+    }
+
+    return {validateButton};
+}
+
+// FUNÇÃO QUE VERIFICA MARKS SELECIONADAS PELOS PLAYERS
+function selectPlayerMarks( markPlayer1Id, markPlayer2Id) {
+
+    const markPlayer1 = document.getElementById(markPlayer1Id);
+    const markPlayer2 = document.getElementById(markPlayer2Id);
+
+    // CLOSURE - GUARDA A LÓGICA DE VERIFICAÇÃO DAS MARCAS DOS PLAYERS
+    const updateMarks = (e) => {
+            
+        if ( e.target === markPlayer1) {
+
+            if( markPlayer1.value === "circle" ) {
+
+                markPlayer2.value = "cross";
+            }
+    
+            else if( markPlayer1.value === "cross" ) {
+    
+                markPlayer2.value = "circle";
+            }
+        }
+
+        else if ( e.target === markPlayer2) {
+                
+            if( markPlayer2.value === "circle" ) {
+
+                markPlayer1.value = "cross";
+            }
+    
+            else if( markPlayer2.value === "cross" ) {
+    
+                markPlayer1.value = "circle";
+            }
+        } 
+    }
+
+    const markPlayer1Validate = () => markPlayer1;
+    const markPlayer2Validate = () => markPlayer2;
+
+    // INSTANCIACAO DAS CLOSURES
+    markPlayer1.addEventListener("change", updateMarks);
+    markPlayer2.addEventListener("change", updateMarks);
+
+    return {markPlayer1Validate, markPlayer2Validate, updateMarks};
+}
+
+function GameController() {
 
     // VARIAVEIS JOGADORES E MARCAS
-    let player1Name = "";
-    let player2Name = "";
-    let player1Mark = "";
-    let player2Mark = "";
+    let player1Name = checkPlayerName( "namePlayer1", "regexMessagePlayer1", "enter").getName();
+    let player2Name = checkPlayerName( "namePlayer2", "regexMessagePlayer2", "enter").getName();
+    let player1Mark = selectPlayerMarks("markPlayer1", "markPlayer2").markPlayer1Validate().value;
+    let player2Mark = selectPlayerMarks("markPlayer1", "markPlayer2").markPlayer2Validate().value;
 
+    const board = Gameboard();
 
-    // VALIDAÇÃO DO NOME DOS JOGADORES
-    function checkPlayerName ( inputId, errorMessageId, buttonEnterId) {
-        
-        const input = document.getElementById(inputId);
-        const errorMessage = document.getElementById(errorMessageId);
-        
-        // O NOME DO JOGADOR DEVE TER ENTRE 3 A 15 LETRAS
-        const regexInput = /^[\p{L}\p{N}]{3,15}$/u;
+    const players = [
+        {
+            name: player1Name,
+            mark: player1Mark
+        },
+        {
+            name: player2Name,
+            mark: player2Mark
+        }
+    ]
 
-        // CLOSURE - LÓGICA DE VALIDAÇÃO DO NOME DO JOGADOR
-        const checkName = () => {
-            
-            const inputUserRegex = regexInput.test(input.value);
+    let activePlayer = players[0].name;
+    let activePlayerMark  = players[0].mark;
 
-            if( input.value.length < 3 || input.value.length > 15 ) {
+    console.log(activePlayer);
 
-                errorMessage.textContent = "Your name should be between 3 and 15 characters";
-                errorMessage.className = "errorMessageIncorrect";
-            }
+    const switchPlayerTurn = () => {
 
-            else if( !inputUserRegex ) {
+        if ( activePlayer === players[0].name) {
 
-                errorMessage.textContent = "Incorrect characters! Please try again";
-                errorMessage.className = "errorMessageIncorrect";
-            }
-
-            else {
-
-                errorMessage.textContent= "Correct Names! Good Luck!";
-                errorMessage.className = "errorMessageCorrect";
-            }
-
-            // INSTANCIA A FUNÇÃO DE DESBLOQUEAR O BOTÃO DE ENTRADA
-            const validatorButton = checkButton(buttonEnterId);
-            
-            validatorButton.validateButton();
+            activePlayer = players[1].name;
+            activePlayerMark = players[1].mark;
         }
 
-        // EXECUTA A FUNÇÃO CHECKNAME
-        function attachEventListener() {
+        else {
 
-            input.addEventListener("input",checkName);
+            activePlayer = players[0].name;
+            activePlayerMark = players[1].mark;
         }
-
-        function deleteEventListener() {
-
-            input.removeEventListener("input",checkName);
-        }
-
-        // RETORNA O VALOR DO JOGADOR
-        function getName() {
-
-            return input.value;
-        }
-
-        return  {attachEventListener, deleteEventListener, getName};
     }
 
-    // FUNÇÃO QUE VERIFICA MARKS SELECIONADAS PELOS PLAYERS
-    function selectPlayerMarks( markPlayer1Id, markPlayer2Id) {
+    const getActivePlayer = () => activePlayer;
+    const getActivePlayerMark = () => activePlayerMark;
 
-        const markPlayer1 = document.getElementById(markPlayer1Id);
-        const markPlayer2 = document.getElementById(markPlayer2Id);
+    const playRound = (column, row) => {
 
-        // CLOSURE - GUARDA A LÓGICA DE VERIFICAÇÃO DAS MARCAS DOS PLAYERS
-        const updateMarks = (e) => {
+        board.placeMark(column, row, getActivePlayerMark());
+        switchPlayerTurn();
+    }
+
+
+    return {getActivePlayer, getActivePlayerMark, playRound, getGameBoard: board.getGameBoard};
+
+}
+
+function ScreenController() {
+
+    // INSTANICAÇÃO DA TABULEIRO
+    const game = GameController();
+    const gameboard = document.getElementById("gameboard");
+    const leaderboard = document.getElementById("leaderboard");
+    const buttons = document.getElementById("buttons");
+
+    const updateScreen = () => {
+
+        // GARANTE QUE NAO EXISTE NADA PREENCHIDO
+        while( gameboard.firstChild) {
+            gameboard.removeChild(gameboard.firstChild);
+        }
+
+        // MONTAR NOVAMNENTE O TABUELEIRO
+        const board = game.getGameBoard();
+
+        // RENDERIZAÇÃO DO TABULEIRO
+        board.forEach( (row, rowIndex) => {
             
-            if ( e.target === markPlayer1) {
-
-                if( markPlayer1.value === "circle" ) {
-
-                    markPlayer2.value = "cross";
-                }
-    
-                else if( markPlayer1.value === "cross" ) {
-    
-                    markPlayer2.value = "circle";
-                }
-            }
-
-            else if ( e.target === markPlayer2) {
+            row.forEach( ( cell, colIndex) => {
                 
-                if( markPlayer2.value === "circle" ) {
+                const cellButton = document.createElement("button");
+                cellButton.classList.add("cel");
+                cellButton.dataset.row = rowIndex;
+                cellButton.dataset.column = colIndex;
+                cellButton.textContent = cell.getMark();
+                gameboard.appendChild(cellButton);
+            })
+        })
 
-                    markPlayer1.value = "cross";
-                }
+        gameboard.style.display="grid";
+        leaderboard.style.display="flex";
+        buttons.style.display="flex";
+    }
+
+    function clickHandlerBoard(e) {
+
+        const selectCelColumn = e.target.dataset.column;
+        const selectCelRow = e.target.dataset.row;
+
+        if (!selectCelColumn || !selectCelRow) return;
+
+        const selectCelColumnValue = parseInt(selectCelColumn);
+        const selectCelRowValue = parseInt(selectCelRow);
+
+        game.playRound(selectCelColumnValue,selectCelRowValue);
+        updateScreen();
+    }
+
     
-                else if( markPlayer2.value === "cross" ) {
+    gameboard.addEventListener("click", clickHandlerBoard);
+
+    updateScreen();
+}
+
+function StartGame() {
+
+    const startButton = document.getElementById("enter");
+    const formWelcome = document.getElementById("formWelcome");
+
+    // Ativa a verificação de nomes
+    const player1Checker = checkPlayerName("namePlayer1", "regexMessagePlayer1", "enter");
+    const player2Checker = checkPlayerName("namePlayer2", "regexMessagePlayer2", "enter");
+
+    player1Checker.attachEventListener();
+    player2Checker.attachEventListener();
+
+    selectPlayerMarks("markPlayer1","markPlayer2");
     
-                    markPlayer1.value = "circle";
-                }
-            } 
-        }
+    startButton.addEventListener("click", () => {
+        player1Checker.deleteEventListener();
+        player2Checker.deleteEventListener();
+        formWelcome.style.display = "none";
+        ScreenController();
+    });
+}
 
-        // INSTANCIACAO DAS CLOSURES
-        markPlayer1.addEventListener("change", updateMarks);
-        markPlayer2.addEventListener("change", updateMarks);
 
-        return {markPlayer1, markPlayer2, updateMarks};
-    }
+document.addEventListener("DOMContentLoaded", StartGame);
 
-    // DESBLOQUEIA O BOTÃO DE ENTRADA
-    function checkButton( buttonEnterId) {
 
-        const regexMessagePlayer1 = document.getElementById("regexMessagePlayer1");
-        const regexMessagePlayer2 = document.getElementById("regexMessagePlayer2");
-        const buttonEnter = document.getElementById(buttonEnterId);
-      
-        //  CLOSURE - LÓGICA DE VALIDAÇÃO
-        const validateButton = () => {
+/*
 
-            if( regexMessagePlayer1.classList.contains("errorMessageCorrect") && regexMessagePlayer2.classList.contains("errorMessageCorrect")) {
 
-                buttonEnter.removeAttribute("disabled");
-                buttonEnter.classList.remove("none");
-                buttonEnter.classList.add("enter");
-            }
 
-            else {
-
-                buttonEnter.setAttribute("disabled", "true");
-                buttonEnter.classList.remove("enter");
-                buttonEnter.classList.add("none");
-            }
-        }
-
-        return {validateButton};
-    }
-
-    // CRIACAO DO TABULEIRO
-    function createGameboard() {
-
-        // DIMENSÕES DO TABULEIRO
-        const cels = 9;
-        const board = [];
-
-        // CONSTRUÇÃO DO TABULEIRO
-        for (let i = 0; i < cels; i++) {
-
-            board[i] = [];
-        }
-
-        // CLOSURE -RETORNA VARIAVEL BOARD
-        const getGameBoard = () => board
-
-        // RETORNA UM OBJETO
-        return {getGameBoard};
-    }
-
-    // INCIA O JOGO
+    // INICIA O JOGO
     function init() {
 
         const buttonEnter = document.getElementById("enter");
@@ -182,8 +358,6 @@ function createGameController() {
             player1Name = player1Checker.getName();
             player2Name = player2Checker.getName();
 
-            console.log(player1Checker.getName());
-
             // ATRIBUIÇÃO DAS MARCAS AO JOGADORES
             player1Mark = document.getElementById("markPlayer1").value;
             player2Mark = document.getElementById("markPlayer2").value;
@@ -192,11 +366,16 @@ function createGameController() {
             player1Checker.deleteEventListener();
             player2Checker.deleteEventListener();
 
-            //REMOVER OS EVENTLISTENER DA VALIDAÇÃO DAS MARCAS
+            // REMOVER OS EVENTLISTENER DA VALIDAÇÃO DAS MARCAS
             selectMarksPlayer.markPlayer1.removeEventListener("change", selectMarksPlayer.updateMarks);
             selectMarksPlayer.markPlayer2.removeEventListener("change", selectMarksPlayer.updateMarks);
-           
-            startGame( player1Name, player2Name, player1Mark, player2Mark );
+            
+            // RENDERIZAÇÃO DO TABULEIRO
+            startGame();
+
+            // DISPONIBILIZA O JOGADOR ATUAL
+            logicRound().getActivePlayer;
+            logicRound().getActivePlayerMark;
         });
     }
 
@@ -230,8 +409,60 @@ function createGameController() {
         leaderboard.style.display = "flex";
         buttons.classList.remove("none");
         formWelcome.classList.add("none");
+
+        // ADICIONA O EVENTLISTENER DE CLIQUE EM CADA CÉLULA
+        const cels = document.querySelectorAll(".cel");
+
+        cels.forEach( cel => {
+
+            cel.addEventListener("click", clickHandlerBoard, {once: true}); //evento só é disparado uma vez 
+        });
+
     }
 
+    // LÓGICA DOS ROUNDS
+    function logicRound() {
+
+        let activePlayer = player1Name;
+        let activePlayerMark = player1Mark;
+
+        const switchPlayerTurn = () => {
+            
+            if ( activePlayer === player1Name ) {
+
+                activePlayer = player2Name;
+                activePlayerMark = player2Mark;
+            }
+
+            else {
+
+                activePlayer = player1Name;
+                activePlayerMark = player1Mark;
+            }
+
+        }
+        
+        const getActivePlayer = () => activePlayer;
+        const getActivePlayerMark = () => activePlayerMark;
+
+        return {getActivePlayer, getActivePlayerMark};
+    }
+
+    // CÉLULA PREENCHIDA
+    function clickHandlerBoard () {
+
+        return (e) => {
+
+            const cell = e.target;
+
+            if ( cell.textContent ===  "") {
+
+                cell.textContent = logicRound().getActivePlayer;
+                switchPlayerTurn()
+            }
+        }
+
+    }
     return {init};
 }
 
@@ -240,4 +471,4 @@ const gameController = createGameController();
 document.addEventListener("DOMContentLoaded", gameController.init());
 
 
-
+*/
--- teste.js
+++ teste.js
@@ -1,140 +1,146 @@
-function createGameController() {
-    let player1Name = '';
-    let player2Name = '';
-    let player1Mark = '';
-    let player2Mark = '';
-
-    function checkPlayerName(inputId, errorMessageId, buttonEnterId) {
-        const input = document.getElementById(inputId);
-        const errorMessage = document.getElementById(errorMessageId);
-        const buttonEnter = document.getElementById(buttonEnterId);
-        const regexInput = /^[\p{L}\p{N}]{3,15}$/u;
-
-        const checkName = () => {
-            const inputUserRegex = regexInput.test(input.value);
-
-            if (input.value.length < 3 || input.value.length > 15) {
-                errorMessage.textContent = "Your name should be between 3 and 15 characters";
-                errorMessage.className = "errorMessageIncorrect";
-            } else if (!inputUserRegex) {
-                errorMessage.textContent = "Incorrect characters! Please try again";
-                errorMessage.className = "errorMessageIncorrect";
-            } else {
-                errorMessage.textContent = "Correct names! Good Luck!";
-                errorMessage.className = "errorMessageCorrect";
-            }
-
-            checkButtons(buttonEnter);
-        };
-
-        function attachEventListener() {
-            input.addEventListener("input", checkName);
-        }
-
-        function removeEventListener() {
-            input.removeEventListener("input", checkName);
-        }
-
-        function getName() {
-            return input.value;
-        }
-
-        return { attachEventListener, removeEventListener, getName };
+function Gameboard() {
+    const rows = 6;
+    const columns = 7;
+    const board = [];
+  
+    for (let i = 0; i < rows; i++) {
+      board[i] = [];
+      for (let j = 0; j < columns; j++) {
+        board[i].push(Cell());
+      }
     }
-
-    function selectMarksPlayer(markPlayer1Id, markPlayer2Id) {
-        const markPlayer1 = document.getElementById(markPlayer1Id);
-        const markPlayer2 = document.getElementById(markPlayer2Id);
-
-        const updateMarks = () => {
-            if (markPlayer1.value === "circle") {
-                markPlayer2.value = "cross";
-            } else if (markPlayer1.value === "cross") {
-                markPlayer2.value = "circle";
-            }
-        };
-
-        markPlayer1.addEventListener("change", updateMarks);
-        markPlayer2.addEventListener("change", updateMarks);
-
-        // AO INICIAR A PÁGINA
-        updateMarks()
-
-        const removeListeners = () => {
-            
-            markPlayer1.removeEventListener("change", updateMarks);
-            markPlayer2.removeEventListener("change", updateMarks);
-        };
-
-        return { updateMarks, removeListeners};
-    }
-
-    function checkButtons(buttonEnter) {
-        const regexMessagePlayer1 = document.getElementById("regexMessagePlayer1");
-        const regexMessagePlayer2 = document.getElementById("regexMessagePlayer2");
-
-        if (
-            regexMessagePlayer1.classList.contains("errorMessageCorrect") &&
-            regexMessagePlayer2.classList.contains("errorMessageCorrect")
-        ) {
-            buttonEnter.removeAttribute("disabled");
-            buttonEnter.classList.remove("none");
-            buttonEnter.classList.add("enter");
-        } else {
-            buttonEnter.setAttribute("disabled", "true");
-            buttonEnter.classList.remove("enter");
-            buttonEnter.classList.add("none");
-        }
-    }
-
-    function init() {
-
-        const buttonEnter = document.getElementById("enter");
-        const player1Checker = checkPlayerName("namePlayer1", "regexMessagePlayer1", "enter");
-        const player2Checker = checkPlayerName("namePlayer2", "regexMessagePlayer2", "enter");
-
-        player1Checker.attachEventListener();
-        player2Checker.attachEventListener();
-
-
-        // DESCONSTRUIR UM OBJECTO
-        const {updateMarks, removeEventListener} = selectMarksPlayer("markPlayer1", "markPlayer2");
-
-        buttonEnter.addEventListener("click", () => {
-            player1Name = player1Checker.getName();
-            player2Name = player2Checker.getName();
-            player1Mark = document.getElementById("markPlayer1").value;
-            player2Mark = document.getElementById("markPlayer2").value;
-
-            startGame( player1Name, player2Name, player1Mark, player2Mark);
-
-            player1Checker.removeEventListener();
-            player2Checker.removeEventListener();
-            removeEventListener();
-        });
-    }
-
-    function startGame() {
-        const board = document.getElementById("gameBoard");
-        const leaderboard = document.getElementById("leaderboard");
-        const buttons = document.getElementById("buttons");
-        const formWelcome = document.getElementById("formWelcome");
-        const playerName1 = document.getElementById("playerName1");
-        const playerName2 = document.getElementById("playerName2");
-
-        playerName1.textContent = player1Name;
-        playerName2.textContent = player2Name;
-
-        board.classList.remove("none");
-        leaderboard.classList.remove("none");
-        leaderboard.style.display = "flex";
-        buttons.classList.remove("none");
-
-        formWelcome.classList.add("none");
-    }
-
-    return { init };
+  
+    const getBoard = () => board;
+  
+    const dropToken = (column, player) => {
+      const availableCells = board.filter((row) => row[column].getValue() === 0).map(row => row[column]);
+  
+      if (!availableCells.length) return;
+  
+      const lowestRow = availableCells.length - 1;
+      board[lowestRow][column].addToken(player);
+    };
+  
+    const printBoard = () => {
+      const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+      console.log(boardWithCellValues);
+    };
+  
+    return { getBoard, dropToken, printBoard };
 }
-
-const gameController = createGameController();
-document.addEventListener("DOMContentLoaded", gameController.init);
+  
+function Cell() {
+    let value = 0;
+  
+    const addToken = (player) => {
+      value = player;
+    };
+  
+    const getValue = () => value;
+  
+    return {
+      addToken,
+      getValue
+    };
+  }
+  
+  function GameController(
+    playerOneName = "Player One",
+    playerTwoName = "Player Two"
+  ) {
+    const board = Gameboard();
+  
+    const players = [
+      {
+        name: playerOneName,
+        token: 1
+      },
+      {
+        name: playerTwoName,
+        token: 2
+      }
+    ];
+  
+    let activePlayer = players[0];
+  
+    const switchPlayerTurn = () => {
+      activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+    const getActivePlayer = () => activePlayer;
+  
+    const printNewRound = () => {
+      board.printBoard();
+      console.log(`${getActivePlayer().name}'s turn.`);
+    };
+  
+    const playRound = (column) => {
+      console.log(
+        `Dropping ${getActivePlayer().name}'s token into column ${column}...`
+      );
+      board.dropToken(column, getActivePlayer().token);
+  
+      /*  This is where we would check for a winner and handle that logic,
+          such as a win message. */
+  
+      switchPlayerTurn();
+      printNewRound();
+    };
+  
+    printNewRound();
+  
+    return {
+      playRound,
+      getActivePlayer,
+      getBoard: board.getBoard
+    };
+  }
+  
+  function ScreenController() {
+    const game = GameController();
+    const playerTurnDiv = document.querySelector('.turn');
+    const boardDiv = document.querySelector('.board');
+  
+    const updateScreen = () => {
+      // clear the board
+      boardDiv.textContent = "";
+  
+      // get the newest version of the board and player turn
+      const board = game.getBoard();
+      const activePlayer = game.getActivePlayer();
+  
+      // Display player's turn
+      playerTurnDiv.textContent = `${activePlayer.name}'s turn...`
+  
+      // Render board squares
+      board.forEach(row => {
+        row.forEach((cell, index) => {
+          // Anything clickable should be a button!!
+          const cellButton = document.createElement("button");
+          cellButton.classList.add("cell");
+          // Create a data attribute to identify the column
+          // This makes it easier to pass into our `playRound` function 
+          cellButton.dataset.column = index
+          cellButton.textContent = cell.getValue();
+          boardDiv.appendChild(cellButton);
+        })
+      })
+    }
+  
+    // Add event listener for the board
+    function clickHandlerBoard(e) {
+      const selectedColumn = e.target.dataset.column;
+      // Make sure I've clicked a column and not the gaps in between
+      if (!selectedColumn) return;
+      
+      game.playRound(selectedColumn);
+      updateScreen();
+    }
+    boardDiv.addEventListener("click", clickHandlerBoard);
+  
+    // Initial render
+    updateScreen();
+  
+    // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
+  }
+  
+  ScreenController();
